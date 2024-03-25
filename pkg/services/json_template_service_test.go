8f9d4221751111e937bf2d826c8d85cc638b9804
ariel.pineiro <ariel.pineiro@iskaypet.com>
2024-03-25 10:17:22 +0100

    mock

--- pkg/services/json_template_service_test.go
+++ pkg/services/json_template_service_test.go
@@ -3,13 +3,17 @@ package services_test
 import (
 	"testing"
 
+
+	mocks "github.com/arielsrv/sdk-cli/pkg/mocks/pkg/services"
+	"github.com/arielsrv/sdk-cli/pkg/model"
 	"github.com/arielsrv/sdk-cli/pkg/services"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestJSONServiceTemplate_GetTemplates(t *testing.T) {
-	gitlabService := services.NewGitLabService()
-	fileSystemService := services.NewFileSystemService()
+	gitlabService := mocks.NewMockGitService(t)
+	fileSystemService := mocks.NewMockTreeService(t)
 	serviceTemplate := services.NewJSONTemplateService(gitlabService, fileSystemService)
 
 	actual := serviceTemplate.GetTemplates()
@@ -24,11 +28,32 @@ func TestJSONServiceTemplate_GetTemplates(t *testing.T) {
 }
 
 func TestJSONServiceTemplate_GetAvailableLanguages(t *testing.T) {
-	gitlabService := services.NewGitLabService()
-	fileSystemService := services.NewFileSystemService()
+	gitlabService := mocks.NewMockGitService(t)
+	fileSystemService := mocks.NewMockTreeService(t)
 	serviceTemplate := services.NewJSONTemplateService(gitlabService, fileSystemService)
 	actual := serviceTemplate.GetAvailableLanguages()
 
 	assert.GreaterOrEqual(t, len(actual), 1)
 	assert.Equal(t, "go", actual[0].Name)
+}
+
+func TestJSONTemplateService_CreateTemplate(t *testing.T) {
+	gitlabService := mocks.NewMockGitService(t)
+	path := "/path/to/template"
+	gitlabService.EXPECT().Clone(&model.Template{
+		Name:          "Backend API SDK",
+		ShortName:     "backend-api-sdk",
+		Language:      "go",
+		RepositoryURL: "git@gitlab.com:iskaypetcom/digital/sre/tools/dev/backend-api-sdk.git",
+		Pattern:       "go-api",
+		Tag:           "v2.3.14",
+	}).Return(&path, nil)
+
+	fileSystemService := mocks.NewMockTreeService(t)
+	fileSystemService.EXPECT().WalkDir(path, "go-api", "hello-api").Return(nil)
+
+	serviceTemplate := services.NewJSONTemplateService(gitlabService, fileSystemService)
+
+	err := serviceTemplate.CreateTemplate("backend-api-sdk", "hello-api")
+	require.NoError(t, err)
 }
